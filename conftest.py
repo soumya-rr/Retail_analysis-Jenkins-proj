62b840abced4aebdb808adbfa75296856b40992d
soumyarbg <soumyarath59@gmail.com>
2025-01-26 22:36:16 +0530

    performing initial commit

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from lib.Utils import get_spark_session
+
+# @pytest.fixture
+# def spark():
+#     return get_spark_session("LOCAL")
+
+# yield id used and above code is re-written to release the resources once unit testing completed
+
+@pytest.fixture
+def spark():
+    "creates spark session"
+    spark_session = get_spark_session("LOCAL")
+    yield spark_session
+    spark_session.stop()
+
+@pytest.fixture
+def expected_results(spark):
+    "gives the expected results"
+    results_schema = "state string, count int"
+    return spark.read \
+        .format("csv") \
+        .schema(results_schema) \
+        .load("data/test_result/state_aggregate.csv")
